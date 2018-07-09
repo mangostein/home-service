3d6386177b471335704525d561349ed3cad73bf0
Daniel Gardner <bowshanks@gmail.com>
2018-07-08 21:23:36 -0600

    redirect out of home-service root

--- src/routes/build.js
+++ src/routes/build.js
@@ -3,8 +3,10 @@ const Example = require('../api/example');
 const ErrorHandler = require('../lib/error-handler');
 const logger = require('../lib/logger')(config);
 
+const rootLocation = process.cwd().replace('/home-service', '');
+
 module.exports = function(app) {
     app.get('/', function(req, res) {
-      res.sendFile('index.html', { root: process.cwd() + '/client/build' });
+      res.sendFile('index.html', { root:  rootLocation + '/client/build' });
     });
 }

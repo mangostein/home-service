f6a23f8da4df226ccec4b659fd9322839ff9ab83
Ben <schinn.ben@gmail.com>
2018-07-09 21:42:40 -0600

    ye

--- src/routes/build.js
+++ src/routes/build.js
@@ -11,6 +11,14 @@ module.exports = function(app) {
     });
 
     app.get('/styles.css', function(req, res) {
-      res.sendFile('style.css', { root:  rootLocation + '/client/build' });
+      res.sendFile('styles.css', { root:  rootLocation + '/client/build' });
+    });
+
+    app.get('/service-worker.js', function(req, res) {
+      res.sendFile('service-worker.js', { root:  rootLocation + '/client/build' });
+    });
+
+    app.get('/main.16c4450d.js', function(req, res) {
+      res.sendFile('main.16c4450d.js', { root:  rootLocation + '/client/build/static/js' });
     });
 }
